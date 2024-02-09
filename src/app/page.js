2fe455581e04c1364ca1b2fd564fba87ebd44bd5
Ankush <deshwalankush23@gmail.com>
2024-02-09 09:27:01 +0530

    bug fixes

--- src/app/page.js
+++ src/app/page.js
@@ -9,14 +9,13 @@ export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col">
       <nav className="flex justify-between max-h-full py-3">
-        <ul className="ml-6 flex flex-row space-x-3 items-center">
-          
+        <ul className="md:ml-6 ml-1flex flex-row space-x-3 items-center">
           <li className="font-serif font-bold text-2xl box-shadow-xl p-2 rounded-lg hover:bg-blue-400 ease-linear duration-300 cursor-pointer">
             Portfolio
           </li>
         </ul>
-        <ul className="flex mr-6 items-center font-serif space-x-4">
-          <li  className="hover:bg-blue-400 p-2 rounded-lg ease-linear duration-300">
+        <ul className="flex md:mr-6 mr-1 items-center font-serif space-x-4">
+          <li className="hover:bg-blue-400 p-2 rounded-lg ease-linear duration-300">
             <Link href="/services">Services</Link>
           </li>
           <li className="hover:bg-blue-400 p-2 rounded-lg ease-linear duration-300">
@@ -29,25 +28,34 @@ export default function Home() {
       </nav>
       <main className="flex md:flex-row flex-col md:justify-around items-center mt-6 ">
         <Image
-        alt="Ankush"
-        src="/Ankush_1mb.jpg"
-        width={200}
-        height={250}
-        className="rounded-full"
-
-         />
-         <div className="md:w-1/2 w-5/6 mt-5 flex flex-col space-y-4">
+          alt="Ankush"
+          src="/Ankush_1mb.jpg"
+          width={200}
+          height={250}
+          className="rounded-full"
+        />
+        <div className="md:w-1/2 w-5/6 mt-5 flex flex-col space-y-4">
           <p className="p-4 ease-linear duration-300">
-            Hi! I am Ankush, a Full-Stack Web Developer with skill set of NEXTJS, TypeScript and MongoDB.
-            I work in MERN Stack totally and provide my services as a freelancer on multiple plateforms.
-            If you want to join me, Connect me with my Social Media Handles. 
+            Hi! I am Ankush, a Full-Stack Web Developer with skill set of
+            NEXTJS, TypeScript and MongoDB. I work in MERN Stack totally and
+            provide my services as a freelancer on multiple plateforms. If you
+            want to join me, Connect me with my Social Media Handles.
           </p>
           <span className="flex flex-row space-x-4">
-            <Link href="https://www.linkedin.com/in/ankush-singh-b0389b27b/" target="_blank"><FaLinkedin /></Link>
-            <Link href="https://github.com/ankush-web-eng/" target="_blank"><RxGithubLogo /></Link>
-            <Link href="https://www.instagram.com/ankush__57s/" target="_blank"><FaInstagram /></Link>
+            <Link
+              href="https://www.linkedin.com/in/ankush-singh-b0389b27b/"
+              target="_blank"
+            >
+              <FaLinkedin />
+            </Link>
+            <Link href="https://github.com/ankush-web-eng/" target="_blank">
+              <RxGithubLogo />
+            </Link>
+            <Link href="https://www.instagram.com/ankush__57s/" target="_blank">
+              <FaInstagram />
+            </Link>
           </span>
-         </div>
+        </div>
       </main>
     </div>
   );
