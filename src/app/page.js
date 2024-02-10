917abead70840d369acfc3e9ea7de356c1704703
Ankush <deshwalankush23@gmail.com>
2024-02-10 10:00:09 +0530

    overflow-y-hidden fixed

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ import { IoLogoCss3 } from "react-icons/io";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col scroll-smooth overflow-x-hidden overflow-y-hidden">
+    <div className="h-screen w-screen flex flex-col scroll-smooth overflow-x-hidden ">
       <nav className="flex justify-between max-h-full py-3">
         <ul className="md:ml-6 ml-1 flex flex-row space-x-3 items-center">
           <li className="font-serif font-bold text-2xl box-shadow-xl p-2 rounded-lg hover:bg-blue-400 ease-linear duration-300 cursor-pointer">
@@ -78,7 +78,7 @@ export default function Home() {
             <BsBoxFill color="black" size={30} />
             <p className="font-bold text-2xl text-black">Tech-Stacks</p>
           </div>
-          <div className="flex items-center justify-start space-x-3 ml-20 mt-3 md:flex-row flex-wrap">
+          <div className="flex items-center justify-start space-x-3 ml-20 mt-3">
             <FaHtml5 color="red" size={25} />
             <IoLogoCss3 color="orange" size={25} />
             <IoLogoJavascript color="yellow" size={25} />
